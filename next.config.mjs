2ad2c59401197ca3d0f13aa8835937cd94c73dc8
Vincent Adittama <adittamavincent@gmail.com>
2026-05-26 01:20:20 +0700

    feat: initialize nextConfig with webpack and onDemandEntries settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      config.watchOptions = {
+        aggregateTimeout: 500,
+        poll: 1000,
+        ignored: ["**/node_modules", "**/.next"],
+      };
+    }
+    return config;
+  },
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 24 * 60 * 60 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 20,
+  },
+};
 
 export default nextConfig;
